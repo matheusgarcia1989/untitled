a85917ac10f9d21cfe924f073acb40aad297637d
Matheus Garcia <matheusgarcia@hotmail.com.br>
2022-11-27 19:10:36 -0300

    Criamos novos métodos de ContarAteDez e contagemRegressiva além de mostrar como utilizar a classe Scanner para ler o que o usuário escolheu no teclado.

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -4,81 +4,120 @@ package intro;
 // 2 - referência as bibliotecas
 
 
+import java.util.Scanner;
 
 // 3 - Classe
 public class Medidas {
-        // 3.1 - Aributos - caracteristicas
+    // 3.1 - Aributos - caracteristicas
 
-        // 3.2 - Métodos e Funções
-        public static void main (String[] args){
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args) {
 
-            // Conditional = verify a condition - check a ask a person, a hardware or softeware
+        // Conditional = verify a condition - check a ask a person, a hardware or softeware
 
-            // switch = selecionar o comportamento do programa conforme a escolha da pessoa ou do software
+        // Utilizar a classe Scanner do Java para ler a escolha do usuário no console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("M E N U   D E   O P Ç Õ E S");
+        System.out.println("c - Calcular Area Modo Curto");
+        System.out.println("e - Calcular Area Modo Extenso");
+        System.out.println("i - If Simples");
+        System.out.println("d - Contar Ate Dez");
+        System.out.println("r - Contagem Regressiva");
+        System.out.println("Qual a opção desejada: ");
+        String opcao = scanner.next();
 
-            String opcao = "melancia";
+        // switch = selecionar o comportamento do programa conforme a escolha da pessoa ou do software
 
-            switch (opcao){
-                case "ifSimples":
-                    System.out.println("Você escolheu executar o método ifSimples");
-                    ifSimples();
-                    break;
-                case "curto":
-                    System.out.println("Você escolheu executar o método calcularAreaModoCurto");
-                    calcularAreaModoCurto();
-                    break;
-                case "extenso":
-                    System.out.println("Você escolheu executar o método calcularAreaModoExtenso");
-                    calcularAreaModoExtenso();
-                    break;
-                default:
-                    System.out.println("Você escolheu outro valor que não tem uma ação associada");
-                    break;
-            }
-
-        }
-        public static void ifSimples(){
-
-            // Conditional = verify a condition - check a ask a person, a hardware or softeware
-            // if = se
-            // else = senão
-
-            String modo = "curto";
-
-            if (modo == "curto"){
+        switch (opcao) {
+            case "c":
+            case "C":
+                System.out.println("Você escolheu executar o método calcularAreaModoCurto");
                 calcularAreaModoCurto();
-            }
-
-            else {
+                break;
+            case "e":
+            case "E":
+                System.out.println("Você escolheu executar o método calcularAreaModoExtenso");
                 calcularAreaModoExtenso();
-            }
-
-
-        }
-        public static void calcularAreaModoCurto(){
-            System.out.println("Cálculo de áreas modo curto");
-
-            int largura = 4;
-            int comprimento = 3;
-
-            System.out.println("Para largura de " + largura + "m" + " e comprimento de " + comprimento + "m" + " a área do tapete é de " + largura * comprimento + "m²");
-
-
-        }
-        public static void calcularAreaModoExtenso(){
-            System.out.println("Cálculo de áreas modo extenso");
-
-            // Calculo de área - Exemplo: o tamanho do tapete ou do piso
-            int largura;
-            int comprimento;
-            int resultado;
-
-            largura = 5; // largura recebe 4 unidades
-            comprimento = 6; // comprimento recebe 3 unidades
-
-            resultado = largura * comprimento;
-            System.out.println("Para largura de " + largura + "m" + " e comprimento de " + comprimento + "m" + " a área do tapete é de " + resultado + "m²");
+                break;
+            case "d":
+            case "D":
+                System.out.println("Você escolheu executar o método contarAteDez");
+                contarAteDez();
+                break;
+            case "i":
+            case "I":
+                System.out.println("Você escolheu executar o método ifSimples");
+                ifSimples();
+                break;
+            case "r":
+            case "R":
+                System.out.println("Você escolheu executar o método Contagem regressiva");
+                contagemRegressiva();
+                break;
+            default:
+                System.out.println("Você escolheu outro valor que não tem uma ação associada");
+                break;
         }
 
+    }
 
+    public static void ifSimples() {
+
+        // Conditional = verify a condition - check a ask a person, a hardware or softeware
+        // if = se
+        // else = senão
+
+        String modo = "curto";
+
+        if (modo == "curto") {
+            calcularAreaModoCurto();
+        } else {
+            calcularAreaModoExtenso();
+        }
+
+
+    }
+
+    public static void calcularAreaModoCurto() {
+        System.out.println("Cálculo de áreas modo curto");
+
+        int largura = 4;
+        int comprimento = 3;
+
+        System.out.println("Para largura de " + largura + "m" + " e comprimento de " + comprimento + "m" + " a área do tapete é de " + largura * comprimento + "m²");
+
+
+    }
+
+    public static void calcularAreaModoExtenso() {
+        System.out.println("Cálculo de áreas modo extenso");
+
+        // Calculo de área - Exemplo: o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5; // largura recebe 4 unidades
+        comprimento = 6; // comprimento recebe 3 unidades
+
+        resultado = largura * comprimento;
+        System.out.println("Para largura de " + largura + "m" + " e comprimento de " + comprimento + "m" + " a área do tapete é de " + resultado + "m²");
+    }
+
+    public static void contarAteDez() {
+        // Loops ou Repetições
+        //
+        System.out.println("Contagem crescente: ");
+        for (int numero = 1; numero <= 10; numero++) {
+            System.out.println(numero);
+        }
+    }
+
+    public static void contagemRegressiva() {
+        System.out.println("Contagem regressiva: ");
+
+        for (int numero = 10; numero > -1; numero--) {
+            System.out.println(numero);
+        }
+    }
 }
